d1a07ccfa90f7657c8ce50c795f8fab18be3a84e
manzim <manzim.ruet@gmail.com>
2021-08-03 00:29:36 +0600

    template team dummy full done, dialogue screen error

---
+++ src/pages/TeamTemplatesDetails.js
@@ -0,0 +1,197 @@
+import team1 from '../resources/screenshots/team.PNG'
+
+export const TeamTemplateDetails = {
+    team1: 
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ,
+    team2: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: 'first draft of ben crellin',
+            teamOf: "ben crellin"
+        }
+    ],
+    team3: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team4: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team5: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team6: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team7: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team8: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team9: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team10: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team11: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team12: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team13: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team14: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+
+    team15: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team16: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team17: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team18: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team19: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team20: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team21: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team22: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team23: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+    team24: [
+        {
+            imgSrc: team1,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+    ],
+}
--- src/pages/Templates.js
+++ src/pages/Templates.js
@@ -4,60 +4,245 @@ import React, { useState } from 'react'
 import '../externalcss/Externalcss.css'
 
 import landingbannerany from '../resources/screenshots/team.PNG'
+import team from '../resources/screenshots/team.PNG'
 
 function Templates() {
+
+    const TeamTemplateDetails = [
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        }
+        ,
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: 'first draft of ben crellin',
+            teamOf: "ben crellin"
+        }
+        ,
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+
+        {
+            imgSrc: team,
+            speciality: 'bruNO team',
+            detailsSpeciality: '3-5-2 formation',
+            teamOf: "my team"
+        },
+    ]
 
     const [isOpen, setIsOpen] = useState(false)
     return (
         <div>
             <div id="templates" className="vh-100 dt w-100 bg-white-20">
                 <div class="dtc v-mid tc white ph3 ph4-l">
-                    <div class="cf w-100 pa2">
-                        <div class="fl w-100 w-50-m  w-25-ns pa2">
-                            <div class="aspect-ratio aspect-ratio--1x1">
-                                <img
-                                    src={landingbannerany}
-                                    alt="landing page banner"
-                                    class="db bg-center cover aspect-ratio--object" />
+                    <div class="cf w-100 pa5">
+                        {TeamTemplateDetails.map((item, id) =>
+                            <div class="fl w-100 w-50-m  w-25-l pa2">
+                                <div class="aspect-ratio aspect-ratio--1x1">
+                                    <img
+                                        src={item.imgSrc}
+                                        alt={item.teamOf}
+                                        class="db bg-center cover aspect-ratio--object" />
+                                </div>
+                                <div href="#0" class="ph2 ph0-l pb3 link db">
+                                    <h3 class="f5 f4-l mb0 black-90"> <strong>Speciality: </strong> {item.speciality} </h3>
+                                    <h3 class="f6 f5 fw4 mt2 black-60"><strong>team of</strong>  {item.teamOf} </h3>
+                                    <Button text="Details" intent="success" minimal large onClick={() => setIsOpen(!isOpen)} />
+                                    <Dialog
+                                        className="bg-red"
+                                        icon="info-sign"
+                                        isOpen={isOpen}
+                                        onClose={() => setIsOpen(!isOpen)}
+                                        title="What's special about this team?"
+                                    >
+                                        <div className={Classes.DIALOG_BODY}>
+                                            <p>
+                                                <strong>
+                                                    {item.detailsSpeciality}
+                                                </strong>
+                                            </p>
+                                            <div className="mw8">
+                                                <img
+                                                    src={item.imgSrc}
+                                                    alt={item.teamOf}
+                                                />            
+                                            </div>
+                                            </div>
+                                            <div className={Classes.DIALOG_FOOTER}>
+                                                <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                                                    <AnchorButton
+                                                        intent={Intent.DANGER}
+                                                        onClick={() => setIsOpen(!isOpen)}
+                                                    >
+                                                        close
+                                                    </AnchorButton>
+                                                </div>
+                                            </div>
+                                    </Dialog>
+                                </div>
+
+                                </div>
+
+                                )
+                        }
+
                             </div>
-                            <div href="#0" class="ph2 ph0-ns pb3 link db">
-                                <h3 class="f5 f4-ns mb0 black-90">Speciality: salah-bruno-kane-TAA quadruple</h3>
-                                <h3 class="f6 f5 fw4 mt2 black-60">team of ben crellin</h3>
-                                <Button text="Details" intent="success" minimal large onClick={() => setIsOpen(!isOpen)} />
-                                <Dialog
-                                    className=""                                  
-                                    icon="info-sign"
-                                    isOpen={isOpen}
-                                    onClose={() => setIsOpen(!isOpen)}
-                                    title="What's special about this team?"
-                                >
-                                    <div className={Classes.DIALOG_BODY}>
-                                        <p>
-                                            <strong>
-                                                Data integration is the seminal problem of the digital age. For over ten years, we’ve
-                                                helped the world’s premier organizations rise to the challenge.
-                                            </strong>
-                                        </p>
-                                    </div>
-                                    <div className={Classes.DIALOG_FOOTER}>
-                                        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                                            <AnchorButton
-                                                intent={Intent.DANGER}
-                                                
-                                                onClick={ () => setIsOpen(!isOpen)}
-                                            >
-                                                close
-                                            </AnchorButton>
-                                        </div>
-                                    </div>
-                                </Dialog>
-                            </div>
-                        </div>
-                    </div>
+                </div>
                 </div>
             </div>
-        </div>
-    )
+            )
 }
 
-export default Templates
+            export default Templates
